91dd3911d838a261705cd436b07d2473971d9a93
juancanchi1992 <juan.canchi@bitlogic.io>
2022-06-12 12:40:19 -0300

    Add test

--- source/cart.go
+++ source/cart.go
@@ -1,16 +1,26 @@
-package source 
-
-import (
-	"fmt"
-)
+package source
 
 type Cart struct {
 	quant int
-}  
+}
 
 // 1. primer carrito que agarro está vacío
 // 2. agregar un libro y los contiene
 // 3. agregar varios libros y los contiene
 // 4. agregar 3x el mismo libro y los contiene
 // 5. quiero agregar 0 o menos libros y no se puede (tira error)
-// 6. 
+// 6.
+
+/*func CreateCart() Cart {
+	newCart := Cart{quant: 0}
+	return newCart
+}*/
+
+func GetCart() (cart []int) {
+	return
+}
+
+func AddObjectToCart(idBook []int) (cart []int) {
+	cart = append(cart, idBook...)
+	return cart
+}
--- test/cart_test.go
+++ test/cart_test.go
@@ -1,19 +1,39 @@
 package test
 
 import (
-	"testing"
+	"TusLibros/source"
 	"github.com/stretchr/testify/suite"
-
+	"testing"
 )
 
 type CartTestSuite struct {
-    suite.Suite
+	suite.Suite
 }
 
+//carrito esta vacio
 func (suite *CartTestSuite) Test001() {
-    cart := 
+	cart := source.GetCart()
+	cantCart := len(cart)
+	suite.Equal(0, cantCart)
 }
 
-func RunCartTestSuite(t *testing.T) {
-    suite.Run(t, new(CartTestSuite))
+//agregar un libro y este lo contiene
+func (suite *CartTestSuite) Test002() {
+	idsBooks := []int{001}
+	cart := source.AddObjectToCart(idsBooks)
+	cantCart := len(cart)
+	suite.Equal(1, cantCart)
+}
+
+//agregar varios libros y este lo contiene
+func (suite *CartTestSuite) Test003() {
+	idsBooks := []int{001, 002}
+	cart := source.AddObjectToCart(idsBooks)
+	cantCart := len(cart)
+	suite.Equal(2, cantCart)
+}
+
+//Esta funcion tiene que comenzar con Test.. por eso no te lo tomaba
+func TestRunCartSuite(t *testing.T) {
+	suite.Run(t, new(CartTestSuite))
 }
