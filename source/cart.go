791221a828da7661a85ca1a4f8d600667e30b4e9
Juan Canchi <juan.canchi@bitlogic.io>
2022-06-13 12:09:49 -0300

    Add catalogue

--- source/cart.go
+++ source/cart.go
@@ -3,9 +3,11 @@ package source
 import (
 	"encoding/json"
 	"errors"
-) 
+)
+
 type Cart struct {
-	Books []Book
+	Books     []Book
+	Catalogue []Book
 }
 
 type Book struct {
@@ -13,6 +15,7 @@ type Book struct {
 }
 
 var Error01 = errors.New("cannot receive 1 items")
+var Error02 = errors.New("book is not part of catalogue")
 
 // 1. primer carrito que agarro está vacío
 // 2. agregar un libro y los contiene
@@ -20,30 +23,35 @@ var Error01 = errors.New("cannot receive 1 items")
 // 4. agregar 3x el mismo libro y los contiene
 // 5. quiero agregar 0 o menos libros y no se puede (tira error)
 
-func NewCart() Cart {
-	newCart := Cart{Books: []Book{}}
+func NewCart(catalogueBooks []Book) Cart {
+	newCart := Cart{Books: []Book{}, Catalogue: catalogueBooks}
 	return newCart
 }
 
-func GetCart() (cart []int) {
-	return
-}
-
 func (c *Cart) AddObjectToCart(aBook Book, numberOfCopies int) (err error) {
-	if(numberOfCopies == 0) {
+	if numberOfCopies == 0 {
 		return Error01
 	}
-	for i := 0; i< numberOfCopies; i++ {
-		c.Books = append(c.Books, aBook)
+
+	flag := false
+	for _, b := range c.Catalogue {
+		if aBook.ISBN == b.ISBN {
+			for i := 0; i < numberOfCopies; i++ {
+				c.Books = append(c.Books, aBook)
+			}
+			flag = true
+		}
 	}
+
+	if flag == false {
+		return Error02
+	}
+
 	return nil
 }
 
 func (c *Cart) ListCartContent() (cartContent string, err error) {
-
-	cartContent, err = json.Marshal(c.Books)
+	cartContentMarshal, err := json.Marshal(c.Books)
+	cartContent = string(cartContentMarshal)
 	return cartContent, err
-
 }
-
-// agregar catálogo!
--- test/cart_test.go
+++ test/cart_test.go
@@ -13,15 +13,15 @@ type CartTestSuite struct {
 
 //carrito esta vacio
 func (suite *CartTestSuite) Test001() {
-    aCart := suite.emptyCart()
+	aCart := suite.emptyCart()
 	cantCart := len(aCart.Books)
 	suite.Equal(0, cantCart)
 }
 
 //agregar un libro y este lo contiene
 func (suite *CartTestSuite) Test002() {
-    aCart := suite.emptyCart()
-    aBook := suite.validBook()
+	aCart := suite.emptyCart()
+	aBook := suite.validBook()
 	aCart.AddObjectToCart(aBook, 1)
 	cantCart := len(aCart.Books)
 	suite.Equal(1, cantCart)
@@ -29,58 +29,71 @@ func (suite *CartTestSuite) Test002() {
 
 //agregar varios libros y este lo contiene
 func (suite *CartTestSuite) Test003() {
-    aCart := suite.emptyCart()
-    aBook := suite.validBook()
-    anotherBook := suite.validBook()
-    aCart.AddObjectToCart(aBook, 1)
-    aCart.AddObjectToCart(anotherBook, 1)
+	aCart := suite.emptyCart()
+	aBook := suite.validBook()
+	anotherBook := suite.validBook()
+	aCart.AddObjectToCart(aBook, 1)
+	aCart.AddObjectToCart(anotherBook, 1)
 	cantCart := len(aCart.Books)
 	suite.Equal(2, cantCart)
 }
 
-
-
-
-
-
 func (suite *CartTestSuite) Test004() {
-    aCart := suite.emptyCart()
-    aBook := suite.validBook()
-    aCart.AddObjectToCart(aBook, 3)
+	aCart := suite.emptyCart()
+	aBook := suite.validBook()
+	aCart.AddObjectToCart(aBook, 3)
 	cantCart := len(aCart.Books)
 	suite.Equal(3, cantCart)
 }
 
 func (suite *CartTestSuite) Test005() {
-    aCart := suite.emptyCart()
-    aBook := suite.validBook()
-    err := aCart.AddObjectToCart(aBook, 0)
+	aCart := suite.emptyCart()
+	aBook := suite.validBook()
+	err := aCart.AddObjectToCart(aBook, 0)
 	cantCart := len(aCart.Books)
-    suite.Equal(0, cantCart)
-    suite.Equal(err, source.Error01)
+	suite.Equal(0, cantCart)
+	suite.Equal(err, source.Error01)
 }
 
-// listar los libros
+//listar los libros
 func (suite *CartTestSuite) Test006() {
-    aCart := suite.emptyCart()
-    aBook := suite.validBook()
-    anotherBook := suite.validBook()
-    aCart.AddObjectToCart(aBook, 1)
-    aCart.AddObjectToCart(anotherBook, 1)
-    cartContent, _ := aCart.ListCartContent()
-    var message = "{}"
-    suite.Equal(message, cartContent)
-
+	aCart := suite.emptyCart()
+	aBook := suite.validBook()
+	anotherBook := suite.validBook()
+	aCart.AddObjectToCart(aBook, 1)
+	aCart.AddObjectToCart(anotherBook, 1)
+	cartContent, _ := aCart.ListCartContent()
+	var message = "[{\"isbn\":\"validBook\"},{\"isbn\":\"validBook\"}]"
+	suite.Equal(message, cartContent)
 }
 
+func (suite *CartTestSuite) Test007() {
+	aCart := suite.emptyCart()
+	aBookValid := suite.validBook()
+	err := aCart.AddObjectToCart(aBookValid, 1)
+	suite.Equal(nil, err)
+}
+
+func (suite *CartTestSuite) Test008() {
+	aCart := suite.emptyCart()
+	aBookNoValid := suite.noValidBook()
+	err := aCart.AddObjectToCart(aBookNoValid, 1)
+	suite.Equal(source.Error02, err)
+}
 
 // Factory Methods
-func (*CartTestSuite) validBook() (source.Book) {
+func (*CartTestSuite) validBook() source.Book {
 	return source.Book{ISBN: "validBook"}
 }
 
-func (*CartTestSuite) emptyCart() (source.Cart) {
-	return source.NewCart()
+func (*CartTestSuite) noValidBook() source.Book {
+	return source.Book{ISBN: "noValidBook"}
+}
+
+func (*CartTestSuite) emptyCart() source.Cart {
+	bookValid := source.Book{ISBN: "validBook"}
+	booksCatalogue := []source.Book{bookValid}
+	return source.NewCart(booksCatalogue)
 }
 
 //Esta funcion tiene que comenzar con Test
